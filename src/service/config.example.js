eef3a7621f115b195b6fa9f5c680886b5d197ce0
LR <erladoisl@gmail.com>
2022-08-11 14:59:04 +0300

    пример конфига firebase

---
+++ src/service/config.example.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "---",
+    authDomain: "goods-gazer.firebaseapp.com",
+    projectId: "goods-gazer",
+    storageBucket: "goods-gazer.appspot.com",
+    messagingSenderId: "---",
+    appId: "---"
+};
+
+export {
+    firebaseConfig,
+};
