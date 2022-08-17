1385cc31d28596d88f672c8575890ceadceb861c
LR <erladoisl@gmail.com>
2022-08-17 15:04:40 +0300

    rf

--- src/components/Content/Header/Header.jsx
+++ src/components/Content/Header/Header.jsx
@@ -6,17 +6,18 @@ import { query, collection, getDocs, where } from "firebase/firestore";
 import { auth, db, logout } from "../../../service/UserService";
 import { useNavigate } from "react-router-dom";
 
-const Header = (() => {
+const Header = () => {
   const [user, loading, error] = useAuthState(auth);
   const [name, setName] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
-    // if (loading) return;
     if (!user) return navigate("/");
-    fetchUserName();
+    if (user) {
+      fetchUserName();
+    }
   }, [user, loading]);
-  
+
   const fetchUserName = async () => {
     try {
       const q = query(collection(db, "users"), where("uid", "==", user?.uid));
@@ -34,43 +35,58 @@ const Header = (() => {
       <Notification />
       <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <div className="container-fluid">
-          <NavLink to='/content' className="navbar-brand">
+          <NavLink to="/content" className="navbar-brand">
             Goods
           </NavLink>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <ul className="navbar-nav me-auto mb-2 mb-md-0">
               <li className="nav-item">
-                <NavLink to='/content' className="nav-link ">
+                <NavLink to="/content" className="nav-link ">
                   Мониторинг
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to='/content' className="nav-link ">
+                <NavLink to="/content" className="nav-link ">
                   База
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to='/content' className="nav-link ">
+                <NavLink to="/content" className="nav-link ">
                   Помощь
                 </NavLink>
               </li>
             </ul>
-            <div className="ms-auto link-light" >
-              <span className="d-sm-inline-block my-2 my-md-0 ms-md-3 text-white" >
-                <NavLink to='/content' className="nav-link text-white">
-                  {name}({user?.email})
-                </NavLink></span>
-              <button className="btn btn-dark d-lg-inline-block my-2 my-md-0 ms-md-3 text-white" onClick={logout}>Выйти</button>
+            <div className="ms-auto link-light">
+              <span className="d-sm-inline-block my-2 my-md-0 ms-md-3 text-white">
+                {!loading && (
+                  <NavLink to="/content" className="nav-link text-white">
+                    {name}({user?.email})
+                  </NavLink>
+                )}
+              </span>
+              <button
+                className="btn btn-dark d-lg-inline-block my-2 my-md-0 ms-md-3 text-white"
+                onClick={logout}
+              >
+                Выйти
+              </button>
             </div>
           </div>
         </div>
       </nav>
     </header>
   );
-});
-
+};
 
 export default Header;
