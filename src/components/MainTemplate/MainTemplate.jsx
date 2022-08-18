7d9006474942eb6c7d405e6dabb19d7666b9c95c
LR <erladoisl@gmail.com>
2022-08-18 14:59:31 +0300

    refactoring

--- src/components/MainTemplate/MainTemplate.jsx
+++ src/components/MainTemplate/MainTemplate.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import Header from '../Content/Header/Header';
-import c from './MainTemplate.module.css';
+import Header from "../Content/Header/Header";
+import c from "./MainTemplate.module.css";
 
 export const MainTemplate = ({ children }) => {
   return (
     <div className={`${c.text_center}`}>
       <Header />
       <div className="cover-container d-flex w-100 h-100 p-md-5 p-1 pt-4 mx-auto flex-column">
-        <div className={c.content}>
-          { children }
-        </div>
+        <div className={c.content}>{children}</div>
       </div>
-    </div >
+    </div>
   );
 };
